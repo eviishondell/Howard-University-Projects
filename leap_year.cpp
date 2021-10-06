3beecba7d861475f878b15c2905828110274a02d
Evolone Layne <53535716+eviishondell@users.noreply.github.com>
2021-10-06 11:58:24 -0400

    Create leap_year.cpp

---
+++ leap_year.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+  
+  int year = 0;
+{cout << "Enter a year:" << endl;
+  cin >> year;
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    cout << year << " is a leap year" << endl;
+  else
+    cout << year << " is not a leap year" << endl;
+  }
+  
+  
+  {cout << "Enter a year:" << endl;
+  cin >> year;
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    cout << year << " is a leap year" << endl;
+  else
+    cout << year << " is not a leap year" << endl;
+  }
+  
+  
+  {cout << "Enter a year:" << endl;
+  cin >> year;
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    cout << year << " is a leap year" << endl;
+  else
+    cout << year << " is not a leap year" << endl;
+  }
+  
+  
+  {cout << "Enter a year:" << endl;
+  cin >> year;
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    cout << year << " is a leap year" << endl;
+  else
+    cout << year << " is not a leap year" << endl;
+  }
+  return 0;
+}
